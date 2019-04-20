212bedba28293a316ce11623f3a027b599509d08
Hui Beom Kim <hkim@wisc.edu>
2019-04-20 12:44:35 -0500

    Added private methods that creates scene

--- application/Main.java
+++ application/Main.java
@@ -1,13 +1,64 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent>{
 	Stage primaryStage;
+	
+	/*- getEnterQuestionPage() : Scene 
+	- getQuestionFilterPage() : Scene
+	- getMainMenuPage() : Scene
+	- getQuestionPage() :  Scene
+	- getResultPage() :  Scene
+	- getSetUpPage() : Scene
+	- getLoadPage():  Scene
+	- getSavePage():  Scene*/
+
+	private Scene getEnterQuestionPage() {
+		
+		return null;
+	}
+	
+	private Scene getQuestionFilterPage() {
+			
+		return null;
+	}
+	
+	private Scene getMainMenuPage() {
+		
+		return null;
+	}
+	
+	private Scene getQuestionPage() {
+		
+		return null;
+	}
+	
+	private Scene getResultPage() {
+		
+		return null;
+	}
+	
+	private Scene getSetUpPage() {
+			
+		return null;
+	}
+	
+	private Scene getLoadPage() {
+		
+		return null;
+	}
+	
+	private Scene getSavePage() {
+		
+		return null;
+	}
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -24,6 +75,11 @@ public class Main extends Application {
 	
 	public void switchTo(Scene scene) {
 		primaryStage.setScene(scene);
+	}
+	
+	@Override
+	public void handle(ActionEvent event) {
+		
 	}
 	
 	public static void main(String[] args) {
