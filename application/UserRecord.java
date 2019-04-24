81f5073d5633fca1d9e1df69c9a57c11ac45a3b5
Nate <nsackett@wisc.edu>
2019-04-23 19:45:35 -0500

    Nate better UserRecord

--- application/UserRecord.java
+++ application/UserRecord.java
@@ -2,14 +2,45 @@ package application;
 import java.util.ArrayList;
 
 public class UserRecord {
-	 ArrayList<String> choices = new ArrayList<String>();
-	 private Integer numQsRqst;
-	 private Integer numQsAns;
-	 private Integer numQsCor;
+	 ArrayList<String> choices;
+	 private Integer numRqst;
+	 private Integer numAns;
+	 private Integer numCor;
 	 
-	 protected void setQs( int qsRqst, int qsAns, int qsCor ) {
-		 numQsRqst = qsRqst;
-		 numQsAns = qsAns;
-		 numQsCor = qsCor;
+	 public UserRecord() {
+		 choices = new ArrayList();
+		 numRqst = 0;
+		 numAns = 0;
+		 numCor = 0;
+	 }
+	 
+	 // Setters
+	 protected void setNumRqst(Integer numRqst) {
+		this.numRqst = numRqst;
+	 }
+	 
+	 protected void setNumAns(Integer numAns) {
+		this.numAns = numAns;
+	 }
+	 
+	 protected void setNumCor(Integer numCor) {
+		this.numCor = numCor;
+	 }
+	 
+	 // Getters
+	 protected Integer getNumRqst() {
+		return numRqst;
+	 }
+	 
+	 protected Integer getNumAns() {
+		return numAns;
+	 }
+	 
+	 protected Integer getNumCor() {
+		return numCor;
+	 }
+	 
+	 protected float getPercent() {
+		return (float) numAns / (float) numCor;
 	 }
 }
