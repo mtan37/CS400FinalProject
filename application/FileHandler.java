50f4f42e4d3913c2f3d764d9cf1cd591e7edd9f0
Nate <nsackett@wisc.edu>
2019-04-23 21:48:13 -0500

    Nate FileHandler update

--- application/FileHandler.java
+++ application/FileHandler.java
@@ -4,35 +4,54 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Hashtable;
+import java.util.ArrayList;
 
-/*import org.json.simple.JSONObject;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;*/
+import org.json.simple.parser.ParseException;
 
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 public class FileHandler {
-	public void readFile() {
-		//try {
-			FileChooser fileChooser = new FileChooser();
-	    	fileChooser.setTitle("Open JSON file");
-	    	fileChooser.setInitialDirectory(new File("./"));
-	    	fileChooser.getExtensionFilters().addAll(new ExtensionFilter("JSON Files", "*.JSON"));
-	    	
-	    	File file = fileChooser.showOpenDialog(null);
+	
+	JSONParser parser;
+	Hashtable< String, ArrayList<Question> > questionBank;
+	
+	
+	public FileHandler(Hashtable< String, ArrayList<Question> > questionBank) {
+		parser = new JSONParser();
+		this.questionBank = questionBank;
+	}
+	
+	public void pickFile() {
+		FileChooser fileChooser = new FileChooser();
+    	fileChooser.setTitle("Open JSON file");
+    	fileChooser.setInitialDirectory(new File("./"));
+    	fileChooser.getExtensionFilters().addAll(new ExtensionFilter("JSON Files", "*.JSON"));
+    	
+    	File file = fileChooser.showOpenDialog(null);
+    	readFile(file);
+	}
+	
+	public boolean readFile(File file) {
+		try {
+
 		
-	    	/*Object obj = new JSONParser().parse(new FileReader(file));
+	    	Object obj = parser.parse(new FileReader(file));
 			JSONObject jo = (JSONObject) obj;
 			System.out.println(obj);
-
+		
+		//TODO:
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ParseException e) {
 			e.printStackTrace();
-		}*/
+		}
+	    return false; // TODO:
 	}
 	
 	public void saveFile() {
