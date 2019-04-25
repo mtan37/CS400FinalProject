ad76e758b70b16b2683262fced9214b3c838b684
Marvin <marvin.tan@wisc.edu>
2019-04-24 21:38:34 -0500

    marvin update5

--- application/Main.java
+++ application/Main.java
@@ -32,6 +32,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 
@@ -184,12 +185,14 @@ public class Main extends Application implements EventHandler<ActionEvent>{
     private Scene setUpAddQuestionPage() {
       BorderPane root = new BorderPane();
       VBox centerVBox = new VBox();
-      
+
       VBox topicBox = new VBox();
       VBox descriptionBox = new VBox();
-      VBox choiceBox = new VBox();
-      HBox TFBox = new HBox();
-      
+
+      VBox choicesBox = new VBox();
+      HBox choiceBox = new HBox();
+      HBox addRemoveDoneBox = new HBox();
+
       HBox topHBox = new HBox();
       HBox bottomHBox = new HBox();
 
@@ -199,31 +202,94 @@ public class Main extends Application implements EventHandler<ActionEvent>{
       Label title = new Label("Quiz Generator");
       title.getStyleClass().add("title");
 
-      // =========
+   // =========
       // Center
       // =========
+
+      // Title
       Label topicLabel = new Label("Topic");
       TextField topicField = new TextField();
       topicBox.getChildren().addAll(topicLabel, topicField);
-
+      // Description
       Label descriptionLabel = new Label("Description");
       TextArea descriptionArea = new TextArea();
+      descriptionBox.setPrefHeight(100);
       descriptionBox.getChildren().addAll(descriptionLabel, descriptionArea);
 
+   // Choice
       Label choiceLabel = new Label("Choices");
       TextField choiceField = new TextField();
-      
-      ToggleButton trueButton = new ToggleButton();
-      trueButton.setText("T");
-      ToggleButton falseButton = new ToggleButton();
-      falseButton.setText("F");
-      
-      TFBox.getChildren().addAll(trueButton, falseButton);
-      
-      choiceBox.getChildren().addAll(choiceLabel, choiceField, TFBox);
+
+
+      Button addButton = new Button();
+      addButton.setText("Add Choice");
+      addButton.setOnAction(event -> {
+          if(choicesBox.getChildren().size() < 6) {
+              HBox newChoiceBox = new HBox();
+              TextField newChoiceField = new TextField();
+
+              newChoiceBox.getChildren().addAll(newChoiceField);
+              HBox.setHgrow(newChoiceField, Priority.ALWAYS);
+              choicesBox.getChildren().add(newChoiceBox);
+          }
+      });
+
+      Button removeButton = new Button();
+      removeButton.setText("Remove Choice");
+      removeButton.setOnAction(event -> {
+          if(choicesBox.getChildren().size() > 2) {
+              choicesBox.getChildren().remove(choicesBox.getChildren().size()-1);
+          }
+      });
+
+      Button doneButton = new Button();
+      doneButton.setText("Done");
+      doneButton.setOnAction(event -> {
+          if(addRemoveDoneBox.getChildren().size() == 3) {
+              addRemoveDoneBox.getChildren().remove(0);
+              addRemoveDoneBox.getChildren().remove(0);
+
+              ToggleGroup toggleGroup = new ToggleGroup();
+              for(int i=1; i<choicesBox.getChildren().size(); i++) {
+                  ToggleButton button = new ToggleButton();
+                  button.setText("F");
+                  button.setToggleGroup(toggleGroup);
+                  button.setOnMouseClicked(events -> {
+                      for(int j=1; j<choicesBox.getChildren().size(); j++){
+                          ((ToggleButton)((HBox)choicesBox.getChildren().get(j)).getChildren().get(1)).setText("F");
+                      }
+                      button.setText("T");
+                  });
+                  ((HBox)choicesBox.getChildren().get(i)).getChildren().add(button);
+              }
+
+              doneButton.setText("Undo");
+          } else if(addRemoveDoneBox.getChildren().size() == 1) {
+              for(int i=1; i<choicesBox.getChildren().size(); i++) {
+                  ((HBox)choicesBox.getChildren().get(i)).getChildren().remove(1);
+              }
+
+              addRemoveDoneBox.getChildren().remove(0);
+              addRemoveDoneBox.getChildren().addAll(addButton, removeButton, doneButton);
+              doneButton.setText("Done");
+          }
+      });
+
+      addRemoveDoneBox.getChildren().addAll(addButton, removeButton, doneButton);
+
+      HBox.setHgrow(choiceField, Priority.ALWAYS);
+      choiceBox.getChildren().addAll(choiceField);
+      choicesBox.getChildren().addAll(choiceLabel, choiceBox);
 
       Button saveButton = new Button();
       saveButton.setText("Save");
+      saveButton.setOnAction(event -> {
+          if(doneButton.getText() == "Undo") {
+              quitPopUp(1);
+          } else if(doneButton.getText() == "Done") {
+              quitPopUp(1);
+          }
+      });
       saveButton.getStyleClass().add("NormalButton");
 
       //saveButton.setOnMouseClicked(event -> quizGenerator.loadFile()); TODO
@@ -236,8 +302,8 @@ public class Main extends Application implements EventHandler<ActionEvent>{
       backButton.getStyleClass().add("backButton");
       
       backButton.setOnAction(event -> {
-        popUpQuitAddQuestion();
-        });
+          quitPopUp(3);
+      });
 
       // Setting up the layout
       topHBox.getChildren().add(title);
@@ -245,8 +311,8 @@ public class Main extends Application implements EventHandler<ActionEvent>{
       topHBox.setPrefHeight(50);
       topHBox.getStyleClass().add("topHBox");
 
-      centerVBox.getChildren().addAll(topicBox, descriptionBox, choiceBox, saveButton);
-      centerVBox.setAlignment(Pos.CENTER);
+      centerVBox.getChildren().addAll(topicBox, descriptionBox, choicesBox, addRemoveDoneBox, saveButton);
+      centerVBox.setPadding(new Insets(30, 20, 30, 20));
       centerVBox.setSpacing(30);
 
       bottomHBox.getChildren().addAll(backButton);
@@ -261,10 +327,90 @@ public class Main extends Application implements EventHandler<ActionEvent>{
       //primaryStage.setScene(sc);
       return sc;
     }
-    
+    /*
+     * Flag:
+     * 1 - Save button in add question page, text filled in
+     * 2 - Save button in add question page, text not filled in
+     * 3 - Back button in add question page, not finished
+     */
+    private void quitPopUp(int flag) {
+      final Stage dialog = new Stage();
+      dialog.initModality(Modality.APPLICATION_MODAL);
+      dialog.initOwner(primaryStage);
+      VBox dialogVbox = new VBox();
+      HBox dialogHbox = new HBox();
+
+      Label prompt = new Label();
+      prompt.getStyleClass().add("smallText");
+
+      Button exit = new Button();
+      exit.getStyleClass().add("popUpButton");
+
+      Button notExit = new Button();
+      notExit.getStyleClass().add("popUpButton");
+
+      // Save button in add question page, text filled in
+      if(flag == 1) {
+          // Someone help me with the prompt text...
+          prompt.setText("Do you want to save?");
+          exit.setText("Yes");
+          notExit.setText("No");
+
+          exit.setOnMouseClicked(event -> {
+              primaryStage.setScene(loadQuestion);
+              dialog.close();
+          });
+          notExit.setOnMouseClicked(event -> {
+              primaryStage.setScene(addQuestion);
+              dialog.close();
+          });
+
+          dialogHbox.getChildren().addAll(exit, notExit);
+          dialogHbox.setAlignment(Pos.CENTER);
+          dialogHbox.setSpacing(10.0);
+          dialogVbox.getChildren().addAll(prompt, dialogHbox);
+          dialogVbox.setAlignment(Pos.CENTER);
+      }
+
+      // Save button in add question page, text not filled in
+      else if(flag == 2) {
+          prompt.setText("You need to fill in the \ninformation before saving");
+
+          dialogVbox.getChildren().add(prompt);
+          dialogVbox.setAlignment(Pos.CENTER);
+      }
+
+      // Back button in add question page, not finished
+      else if(flag == 3) {
+          prompt.setText("Are you sure you want to exit without finishing editing?");
+          exit.setText("Yes,exit");
+          notExit.setText("Keep editing");
+
+          exit.setOnMouseClicked(event -> {
+              primaryStage.setScene(loadQuestion);
+              dialog.close();
+          });
+          notExit.setOnMouseClicked(event -> {
+              primaryStage.setScene(addQuestion);
+              dialog.close();
+          });
+
+          dialogHbox.getChildren().addAll(exit, notExit);
+          dialogHbox.setAlignment(Pos.CENTER);
+          dialogHbox.setSpacing(10.0);
+          dialogVbox.getChildren().addAll(prompt, dialogHbox);
+          dialogVbox.setAlignment(Pos.CENTER);
+      }
+
+      Scene dialogScene = new Scene(dialogVbox, 400, 100);
+      dialogScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+      dialog.setScene(dialogScene);
+      dialog.show();
+    }
     //Lucy
+	//Marvin
 	private Scene setUpQuestionFilterPage() {
-	//count number of rows in the grid
+	    //count number of rows in the grid
 	    BorderPane root= new BorderPane();// the root layout
 	    HBox centerHBox = new HBox();
 	    HBox topHBox = new HBox();
@@ -366,7 +512,7 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 
 	}
 
-  private void addBtQuestionFilter(String numTxt) {
+    private void addBtQuestionFilter(String numTxt) {
     int numQuestion;
     try {
     numQuestion = Integer.parseInt(numTxt);
@@ -615,6 +761,7 @@ public class Main extends Application implements EventHandler<ActionEvent>{
         quizGenerator.userRecord.setNumRqst(0);
         quizGenerator.userRecord.setNumAns(0);
         quizGenerator.userRecord.setNumCor(0);
+        quizGenerator.userRecord.setCurrQuizSaved(false);
         /*reset 1.numAnswered
          *      2.numCorrect
          *      3.numCorrectPercentage
@@ -624,6 +771,7 @@ public class Main extends Application implements EventHandler<ActionEvent>{
         primaryStage.setScene(questionFilter);
         quizGenerator.userRecord.setNumAns(0);
         quizGenerator.userRecord.setNumCor(0);
+        quizGenerator.userRecord.setCurrQuizSaved(false);
         /*reset 1.numAnswered
          *      2.numCorrect
          *      3.numCorrectPercentage
