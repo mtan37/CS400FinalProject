f434737bb67e057136bb94b6734c4e726f0a2581
Zhengyi Chen <zchen597@wisc.edu>
2019-04-26 17:22:17 -0500

    exception

--- application/corAnsRemovedException.java
+++ application/corAnsRemovedException.java
@@ -1,8 +1,13 @@
 package application;
 
 
-@SuppressWarnings("serial")
+//@SuppressWarnings("serial")
 public class corAnsRemovedException extends Exception {
+    /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
     @Override
     public String getMessage() {
         return "The correct choice is going to be removed. "
